fb14a9a421c1b3002506c7ef8b0a886b742838fe
MatteoRuffa <cortese_17@protonmail.com>
2024-05-07 17:51:58 +0200

    small changes

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 include __DIR__ ."/Views/header.php";
 include __DIR__ ."/controllers/hotels.php";
 
-if (!empty($_GET['dataParking']) || (isset($_GET['dataParking']) && $_GET['dataParking'] == 0)) {
-    $dataParking = $_GET['dataParking'];
-    $hotelsParking = array_filter($hotels, function ($hotel) use ($dataParking) {
-        return $hotel['parking'] == $dataParking || $dataParking == "all";
-    });
-} else {
-    $hotelsParking = $hotels;
-}
+// if (!empty($_GET['dataParking']) || (isset($_GET['dataParking']) && $_GET['dataParking'] == 0)) {
+//     $dataParking = $_GET['dataParking'];
+//     $hotelsParking = array_filter($hotels, function ($hotel) use ($dataParking) {
+//         return $hotel['parking'] == $dataParking || $dataParking == "all";
+//     });
+// } else {
+//     $hotelsParking = $hotels;
+// }
 
 
 ?>
